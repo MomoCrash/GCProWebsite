a5ad5fe86fea7249d2cbecb3d688f77ec8644888
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2024-02-01 10:59:02 +0100

    anim video

---
+++ js/home.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector(".popup_btn")
+let actTrailer = document.querySelector(".trailer")
+btn.onclick = function() {
+    btn.classList.add("active")
+    actTrailer.classList.add("active")
+}
