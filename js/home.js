faf580f84823e794badfd72afd3da6a4905d4c84
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2024-02-02 12:19:31 +0100

    video fonctionnellle

--- js/home.js
+++ js/home.js
@@ -1,6 +1,24 @@
-let btn = document.querySelector(".popup_btn")
+let btn = document.querySelector(".img_trailer")
 let actTrailer = document.querySelector(".trailer")
+let line = document.querySelector(".line")
+let video = document.getElementById("video")
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+window.onclick = function() {
+    if (actTrailer.classList.contains("active")) {
+        actTrailer.classList.toggle("active")
+        actTrailer.pause()
+    }
+  };
 btn.onclick = function() {
-    btn.classList.add("active")
-    actTrailer.classList.add("active")
+    if (!actTrailer.classList.contains("active")) {
+        line.style.zIndex = "1000";
+        line.classList.toggle("active");
+        sleep(4000).then(() => {actTrailer.classList.toggle("active"); 
+                                video.play(); 
+                                line.classList.toggle("active");
+                            })
+    }
 }
