425252ca203d277f8a18276ed12152d971878639
NetroxTTV <147041723+NetroxTTV@users.noreply.github.com>
2024-02-06 11:55:37 +0100

    oui

--- new.php
+++ new.php
@@ -8,43 +8,46 @@
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/newstyle.css">
     <link rel="stylesheet" href="css/style.css">
-    <link href="css/style2.css" rel="stylesheet" type="text/css"/>
+    <link href="css/style2.css" rel="stylesheet" type="text/css" />
 
     <title>Les News</title>
 </head>
 
 
 <body class="fond_page_news">
-        <nav class="navbar" id="navbar">
-            <div class="nav-img">
-                <a href="#"><img src="ressources/logo_black.svg" alt="logo home"></a>
-            </div>
-            <div class="nav-links">
-                <ul>
+    <nav class="navbar" id="navbar">
+        <div class="nav-img">
+            <a href="#"><img src="ressources/logo_black.svg" alt="logo home"></a>
+        </div>
+        <div class="nav-links">
+            <ul>
                 <li><a href="#">NEWS</a></li>
                 <li><a href="#" class="nav-border">NOS EXPERIENCES</a></li>
                 <li><a href="#" class="nav-border">A PROPOS DE NOUS</a></li>
                 <li><a href="#" class="nav-border">NOS EQUIPEMENTS</a></li>
                 <li><a href="#" id="login" class="nav-border"><b>CONNEXION</b></a></li>
-                </ul>
-            </div>
-            <img src="ressources/bouton_menu_by_moi.png" alt="menu_bouton" class="menu_bouton" id="menu_bouton">
-        </nav>
-        <header></header>
-        <div class="img-header"><img src="ressources/EN TÊTE.png" alt="header"></div>
+            </ul>
+        </div>
+        <img src="ressources/bouton_menu_by_moi.png" alt="menu_bouton" class="menu_bouton" id="menu_bouton">
+    </nav>
+    <header></header>
+    <div class="img-header"><img src="ressources/EN TÊTE.png" alt="header"></div>
     <div class="loginbg">
-    <form action="account.php?method=login&redirect='darkroom/dark_room1.php'" method="POST" class="login-pan mb-3" style="width: 0px; height: 0px; z-index: 1; visibility: hidden;">
-          <div class="login-content">
-              <p><SPAN STYLE="color:#000000"><b>Connexion</b></span></p>
-              <p> Identifiant <input class="login-input form-label" type="email" name="email" placeholder="abc@mail.fr"> </input>
-              </p>
-              <p> Mot de passe <input class="login-input form-label" type="password" name="password" placeholder="······"> </input>
-              </p>
-              <a style="gray" href="../account.php?method=register'">Crée un compte</a>      <button type="submit" name="submit"
-                  placeholder="Connexion">Connexion</button>
-          </div>
-    </form>
-  </div>
+        <form action="account.php?method=login&redirect='darkroom/dark_room1.php'" method="POST" class="login-pan mb-3"
+            style="width: 0px; height: 0px; z-index: 1; visibility: hidden;">
+            <div class="login-content">
+                <p><SPAN STYLE="color:#000000"><b>Connexion</b></span></p>
+                <p> Identifiant <input class="login-input form-label" type="email" name="email"
+                        placeholder="abc@mail.fr"> </input>
+                </p>
+                <p> Mot de passe <input class="login-input form-label" type="password" name="password"
+                        placeholder="······"> </input>
+                </p>
+                <a style="gray" href="../account.php?method=register'">Crée un compte</a> <button type="submit"
+                    name="submit" placeholder="Connexion">Connexion</button>
+            </div>
+        </form>
+    </div>
 
     <main>
         <!-- Main content sections -->
@@ -191,31 +194,31 @@
 
     <body>
         <footer>
-                <div class="footer-nav">
-                    <a href="#"><span>Nous contacter</span></a>
-                    <a href="#"><span>Réservation</span></a>
-                    <a href="#"><span>FAQ</span></a>
-                </div>
-                <div class="copyright">
-                    © THE SENSE, SAS. Tous droits réservés
-                </div>
-                <div class="moda">
-                    <a href="#"><span>Modalités</span></a>
-                    <span> | </span>
-                    <a href="#"><span>Politique de confidentialité </span></a>
-                    <div class="contact">
+            <div class="footer-nav">
+                <a href="#"><span>Nous contacter</span></a>
+                <a href="#"><span>Réservation</span></a>
+                <a href="#"><span>FAQ</span></a>
+            </div>
+            <div class="copyright">
+                © THE SENSE, SAS. Tous droits réservés
+            </div>
+            <div class="moda">
+                <a href="#"><span>Modalités</span></a>
+                <span> | </span>
+                <a href="#"><span>Politique de confidentialité </span></a>
+                <div class="contact">
                     <span> | </span>
                     <a href="#"><span>Contact</span></a>
-                    </div>
-                    
                 </div>
-                <div class="social-icons">
-                    <a href="#"><img src="ressources/Youtube.png" alt="Youtube"></a>
-                    <a href="#"><img src="ressources/Instagram.png" alt="Instagram"></a>
-                    <a href="#"><img src="ressources/Twitter.png" alt="Twitter"></a>
-                    <a href="#"><img src="ressources/Facebook.png" alt="Facebook"></a>
-                </div>
-            </footer>
+
+            </div>
+            <div class="social-icons">
+                <a href="#"><img src="ressources/Youtube.png" alt="Youtube"></a>
+                <a href="#"><img src="ressources/Instagram.png" alt="Instagram"></a>
+                <a href="#"><img src="ressources/Twitter.png" alt="Twitter"></a>
+                <a href="#"><img src="ressources/Facebook.png" alt="Facebook"></a>
+            </div>
+        </footer>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
             integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
         </script>
