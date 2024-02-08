d1d4cbc7dfd88196bb3c1ac5805f5350df96325a
Lootchy <zcolucci@gaming.tech>
2024-02-08 22:34:02 +0100

    apropos footer Update

--- Apropos.php
+++ Apropos.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"></noscript>
     <link href="css/apropos.css" rel="stylesheet" type="text/css"/>
     <link href="css/mainstyle.css" rel="stylesheet" type="text/css"/>
     <title>A propos</title>
